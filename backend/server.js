e3bfa0d831bb447551cc8620936d26faf56a5171
ernestobone98 <ernestobone98>
2023-12-31 01:20:31 +0100

    api/customers works

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,43 @@
-//import pg from lodash
-// import Client from 'lodash/pg';
+const express = require('express');
 const { Client } = require('pg');
+
+const app = express();
+const port = 3000;
 
 // Replace the connection string with your own
 const connectionString = 'postgres://postgres:AglaeSebastien@135.181.84.87:5432/mbds_bigdata?sslmode=disable';
 
-// Create a new PostgreSQL client
 const client = new Client({
   connectionString: connectionString,
 });
 
-// Connect to the PostgreSQL database
-client.connect()
-  .then(() => {
+app.get('/api/customers', async (req, res) => {
+  try {
+    // Connect to the PostgreSQL database
+    await client.connect();
+
     console.log('Connected to the database');
-    
-    // Now you can execute queries or perform other database operations
-    
-    // Example: Execute a simple query
-    return client.query('SELECT * FROM catalog_car');
-  })
-  .then((result) => {
+
+    // Execute a simple query
+    const result = await client.query('SELECT * FROM customer');
+
     console.log('Query result:', result.rows);
-  })
-  .catch((error) => {
+
+    res.json(result.rows);
+  } catch (error) {
     console.error('Error connecting to the database:', error);
-  })
-  .finally(() => {
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
     // Close the connection when done
-    client.end()
-      .then(() => console.log('Connection closed'))
-      .catch((error) => console.error('Error closing connection:', error));
-  });
+    try {
+      await client.end();
+      console.log('Connection closed');
+    } catch (error) {
+      console.error('Error closing connection:', error);
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
